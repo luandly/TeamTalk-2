b12b74a107c5c505efbb8f73fd592e2809245a0c
luandly <luandly@users.noreply.github.com>
2023-03-27 00:53:15 +0800

    插入群成员表
                        //IMGroupMember 插入群成员表

--- server/src/db_proxy_server/business/GroupModel.cpp
+++ server/src/db_proxy_server/business/GroupModel.cpp
@@ -367,6 +367,7 @@ bool CGroupModel::insertNewMember(uint32_t nGroupId, set<uint32_t>& setUsers)
                         strSql = "update IMGroupMember set status=0, updated="+int2string(nCreated)+" where groupId=" + int2string(nGroupId) + " and userId in (" + strClause + ")";
                         pDBConn->ExecuteUpdate(strSql.c_str());
                     }
+                    //IMGroupMember 插入群成员表
                     strSql = "insert into IMGroupMember(`groupId`, `userId`, `status`, `created`, `updated`) values\
                     (?,?,?,?,?)";
                     
